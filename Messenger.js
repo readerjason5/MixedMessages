8c9cf42878d043919f52f3f2c1b641f551eb17c4
Jason Reader <36717169+readerjt@users.noreply.github.com>
2021-08-18 16:06:55 -0400

    Added random message functionality

--- Messenger.js
+++ Messenger.js
@@ -1,1 +1,8 @@
-console.log("Welcome!");
+const motivationalQuotes = ["The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty", "Don’t let yesterday take up too much of today",
+"When we strive to become better than we are, everything around us becomes better too", "Setting goals is the first step in turning the invisible into the visible.", 
+"It is never too late to be what you might have been.", "Just one small positive thought in the morning can change your whole day", "Opportunities don't happen, you create them.",
+"I am not a product of my circumstances. I am a product of my decisions."];
+
+let ranNum = Math.floor(Math.random() * 8);
+
+console.log(motivationalQuotes[ranNum]);
